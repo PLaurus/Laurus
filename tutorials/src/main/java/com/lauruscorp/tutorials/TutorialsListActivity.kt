cbae7d9b0c1fc8a2e45a45c95b8df5f00ce45af4
Lavrikov Pavel <laurus.p.a.work@gmail.com>
2023-04-23 19:51:05 +0300

    Created tutorials app

---
+++ tutorials/src/main/java/com/lauruscorp/tutorials/TutorialsListActivity.kt
@@ -0,0 +1,11 @@
+package com.lauruscorp.tutorials
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TutorialsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorials_list)
+    }
+}
